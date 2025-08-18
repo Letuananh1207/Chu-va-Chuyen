36b6847da89cb4ad624ad35e8dcf49b3ddd4324d
Letuananh1207 <tuananh12072002@gmail.com>
2025-08-19 04:33:09 +0700

    Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Chu-va-Chuyen">
       <App />
     </BrowserRouter>
   </React.StrictMode>
